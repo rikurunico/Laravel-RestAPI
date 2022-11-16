6f291133fd64a6c88dfe00f02be7066d2f20bfea
nicoco1173 <rikurunico@gmail.com>
2022-11-16 21:04:51 +0700

    Stored Request Validation

--- app/Http/Controllers/QuotesControllerAPI.php
+++ app/Http/Controllers/QuotesControllerAPI.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreQuoteRequest;
 use App\Http\Resources\QuoteResource;
 use App\Models\quote;
 use Illuminate\Http\Request;
@@ -24,9 +25,9 @@ class QuotesControllerAPI extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreQuoteRequest $request)
     {
-        //
+        return response()->json('hello');
     }
 
     /**
