b74e063e14a995a3e7ee9c6756efe2788c2d2e0e
nicoco1173 <rikurunico@gmail.com>
2023-01-09 17:08:35 +0700

    Refactor

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -6,29 +6,27 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Resources\LoginResource;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class ApiAuthController extends Controller
 {
     public function login(LoginRequest $request)
     {
-
-        $user = User::where('email', $request->email)->first();
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            return response([
-                'message' => ['These credentials do not match our records.']
+        if (Auth::attempt($request->only('email', 'password'))) {
+            //get user info
+            $user = User::where('email', $request->email)->first();
+            //generate Token
+            $token = $user->createToken('my-app-token')->plainTextToken;
+            //return response json
+            return new LoginResource([
+                'token' => $token,
+                'user' => $user,
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'Data yang di masukkan salah'
             ], 401);
         }
-
-        //generate Token
-        $token = $user->createToken('my-app-token')->plainTextToken;
-
-        //return response json
-        return new LoginResource([
-            'token' => $token,
-            'user' => $user,
-        ]);
     }
 
     public function register(Request $request)
